1eb0da12de3b512bb6e853e0e5e0facc838f2ffc
Thomas <thomascaycedo19@outlook.com>
2019-04-13 21:41:34 -0500

    Se implemeta hbs para hacer paginas dinamicas

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express')
 const app = express();
- 
-app.get('/', function (req, res) {
 
-    let salida = {
-        nombre: 'Thomas',
-        edad: 20,
-        url: req.url
-    };
+app.use( express.static(__dirname + '/public/'))
 
-  res.send(salida);
+app.set('view engine', 'hbs');
+
+app.get('/',(req,res)=>{
+    res.render('home',{
+        nombre: 'Thomas Caycedo Martinez',
+        year: new Date().getFullYear()
+    })
 })
- 
+
+
 app.listen(3000, ()=> {
     console.log('Escuhando peticiones en el puerto 3000')
 })
